315851b79f633b12194ea57aedcff806d05e55b7
sarank1634 <saravanakumar1634@gmail.com>
2025-08-07 17:14:54 +0530

    play button fix after adding track and timing

--- spotify_clone/src/components/Player.jsx
+++ spotify_clone/src/components/Player.jsx
@@ -1,36 +1,38 @@
 import { useContext } from "react";
-import { assets, songsData } from "../assets/assets";
+import { assets,  } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 
 const Player = () => {
 
-    const {seekBar, seekBg, playStatus, Play, Pause} = useContext(PlayerContext)
+    const {track,seekBar,seekBg,playStatus,Play,Pause,time} = useContext(PlayerContext)
     
     return(
         <div className="h-[15%] bg-black flex justify-between items-center text-white px-4 ">
               <div className="lg:flex items-center gap-4">
-                <img className="w-12" src={songsData[0].image} alt="" />
+                <img className="w-12" src={track.image} alt="" />
                 <div>
-                    <p>{songsData[0].name}</p>
-                    <p>{songsData[0].desc.slice(0,12)}</p>
+                    <p>{track.name}</p>
+                    <p>{track.desc.slice(0,12)}</p>
                 </div>
               </div>
               <div className="flex flex-col items-center gap-4 m-auto">
                 <div className="flex gap-4">
                     <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="" />
                     <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
-                    <img onClick={Play} className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
-                    <img onClick={Pause} className="w-4 cursor-pointer" src={assets.pause_icon} alt="" />
+                    {playStatus ?
+                     <img onClick={Pause} className="w-4 cursor-pointer" src={assets.pause_icon} alt="" /> //use ternary oprator to check play or not
+                    : <img onClick={Play} className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+                    }
                     <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
                     <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
                 </div>
                 <div ref={seekBg} className="flex items-center gap-5">
-                    <p>1:06</p>
+                    <p>{time.currentTime.minute}:{time.currentTime.second}</p>
                     <div className="w-[60vw] max-w-[500px] bg-gray-800 rounded-full cursor-pointer">
                        <hr ref={seekBar} className="h-1  w-0 bg-green-800 rounded-full" /> 
                     </div>
-                    <p>3:20</p>
+                    <p>{time.totalTime.minute}:{time.totalTime.second}</p>
                 </div>
               </div>
               <div cla ssName="flex items-center gap-2 opacity-75">
